3f81f36a96ac79de69a94788a138c4e40556e4f0
Luiz Santiago <123255082+luizsant@users.noreply.github.com>
2023-12-21 17:00:26 +0000

    atualizando, incluindo home page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,13 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { MenuBarComponent } from './componentes/menu-bar/menu-bar.component';
-import { MenuTitleComponent } from './componentes/menu-title/menu-title.component';
-import { BigCardComponent } from './componentes/big-card/big-card.component';
-import { SmallCardComponent } from './componentes/small-card/small-card.component';
+import { MenuBarComponent } from './components/menu-bar/menu-bar.component';
+import { MenuTitleComponent } from './components/menu-title/menu-title.component';
+import { BigCardComponent } from './components/big-card/big-card.component';
+import { SmallCardComponent } from './components/small-card/small-card.component';
+import { HomeComponent } from './pages/home/home.component';
 
 @NgModule({
-  declarations: [AppComponent, MenuBarComponent, MenuTitleComponent, BigCardComponent, SmallCardComponent],
+  declarations: [AppComponent, MenuBarComponent, MenuTitleComponent, BigCardComponent, SmallCardComponent, HomeComponent],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
